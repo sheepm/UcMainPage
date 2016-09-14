a3200e8a40ba2b3f7c9dcb91ba09763d00e7e6e2
paomo <redswing@foxmail.com>
2016-09-14 12:00:02 +0800

    add adapter

--- UcClient/app/src/main/java/sheepm/ucclient/adapter/ContentAdapter.java
+++ UcClient/app/src/main/java/sheepm/ucclient/adapter/ContentAdapter.java
@@ -1,33 +1,57 @@
 package sheepm.ucclient.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import sheepm.ucclient.R;
 
 /**
  * Created by paomo on 2016/9/14.
  */
 public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.NormalViewHolder>{
 
+    private List<String> mLists;
+    private Context mContext;
+
+    public ContentAdapter(Context context , List<String> list){
+        this.mContext = context;
+        this.mLists = list;
+    }
+
     @Override
     public NormalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.layout_item_content,parent,false);
+        NormalViewHolder holder = new NormalViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(NormalViewHolder holder, int position) {
-
+        String s = mLists.get(position);
+        if(!TextUtils.isEmpty(s)){
+            holder.mTvName.setText(s);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mLists.size();
     }
 
     public static class NormalViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView mTvName;
+
         public NormalViewHolder(View itemView) {
             super(itemView);
+            mTvName = (TextView) itemView.findViewById(R.id.tv_content_name);
         }
     }
 
--- UcClient/app/src/main/java/sheepm/ucclient/fragment/MainFragment.java
+++ UcClient/app/src/main/java/sheepm/ucclient/fragment/MainFragment.java
@@ -3,22 +3,48 @@ package sheepm.ucclient.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import sheepm.ucclient.R;
+import sheepm.ucclient.adapter.ContentAdapter;
 
 /**
  * Created by paomo on 2016/9/13.
  */
 public class MainFragment extends Fragment {
 
+    private ContentAdapter mAdapter;
+    private RecyclerView mRvContent;
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.page_view_one,container,false);
+        view = inflater.inflate(R.layout.page_view_one,container,false);
+        initView();
         return view;
+    }
+
+    private void initView(){
+        mAdapter = new ContentAdapter(getActivity(),getData());
+        mRvContent = (RecyclerView) view.findViewById(R.id.rv_content);
+        mRvContent.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRvContent.setAdapter(mAdapter);
+    }
+
+    private List<String> getData(){
+        List<String> list = new ArrayList<>();
+        for (int i = 0 ; i < 20 ; i++){
+            list.add("item" + i);
+        }
+        return list;
     }
 
     public static MainFragment newInstance(){
